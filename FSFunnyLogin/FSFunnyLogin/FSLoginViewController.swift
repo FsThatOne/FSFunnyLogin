5cb74bb2d20d7e64adaf2895070870c1cacdb43f
FsThatOne <fsymbio@icloud.com>
2015-10-30 19:42:45 +0800

    setupUI_2

--- FSFunnyLogin/FSFunnyLogin/FSLoginViewController.swift
+++ FSFunnyLogin/FSFunnyLogin/FSLoginViewController.swift
@@ -14,21 +14,9 @@ enum FsLoginTypeShow{
     case FsLoginTypeShow_Pass
 }
 
-let vLogin = UIView(frame: CGRect(x: 15, y: 200, width: mainSize.width - 30, height: 160));
-
-let imgLogin = UIView(frame: CGRect(x: mainSize.width / 2 - 211 / 2, y: 100, width: 211, height: 109))
-
 let mainSize = UIScreen.mainScreen().bounds.size
 
 let offsetLeftHand = 60
-
-let rectLeftHand = CGRect(x: 61 - offsetLeftHand, y: 90, width: 40, height: 70)
-
-let rectRightHand = CGRect(x: imgLogin.frame.size.width / 2 + 60, y: 90, width: 40, height: 70)
-
-let rectLeftHandGone = CGRect(x: mainSize.width / 2 - 100, y: vLogin.frame.origin.y - 22, width: 40, height: 40)
-
-let rectRightHandGone = CGRect(x: mainSize.width / 2 + 62,y: vLogin.frame.origin.y - 22, width: 40, height: 40)
 
 class FSLoginViewController: UIViewController , UITextFieldDelegate {
 
@@ -55,27 +43,17 @@ class FSLoginViewController: UIViewController , UITextFieldDelegate {
         let loginImg = UIImageView(frame: CGRect(x: mainSize.width / 2 - 211 / 2, y: 100, width: 211, height: 109))
         loginImg.image = UIImage(named: "owl-login")
         loginImg.layer.masksToBounds = true
-        self.view.addSubview(loginImg)
-        
-        //左手图1
-        let leftHandImg = UIImageView(frame: rectLeftHand)
-        leftHandImg.image = UIImage(named: "icon_hand")
-        loginImg.addSubview(leftHandImg)
-        
-        //右手图1
-        let rightHandImg = UIImageView(frame: rectRightHand)
-        rightHandImg.image = UIImage(named: "icon_hand")
-        loginImg.addSubview(rightHandImg)
+        view.addSubview(loginImg)
         
         //输入框容器视图
         let loginView = UIView(frame: CGRect(x: 15, y: 200, width: mainSize.width - 30, height: 160))
         loginView.layer.borderWidth = 1
         loginView.layer.borderColor = UIColor.blackColor().CGColor
         loginView.backgroundColor = UIColor.whiteColor()
-        self.view.addSubview(loginView)
+        view.addSubview(loginView)
         
         //用户名
-        let userName = UITextField(frame: CGRect(x: 30, y: 30, width: vLogin.bounds.width - 60, height: 44))
+        let userName = UITextField(frame: CGRect(x: 30, y: 30, width: loginView.frame.width - 60, height: 44))
         userName.delegate = self
         userName.layer.cornerRadius = 5;
         userName.layer.borderColor = UIColor.lightGrayColor().CGColor;
@@ -87,8 +65,9 @@ class FSLoginViewController: UIViewController , UITextFieldDelegate {
         userName.leftView?.addSubview(imgUser)
         loginView.addSubview(userName)
         
+        
         //密码
-        let passWord = UITextField(frame: CGRect(x: 30, y: 90, width: vLogin.bounds.width - 60, height: 44))
+        let passWord = UITextField(frame: CGRect(x: 30, y: 90, width: loginView.frame.width - 60, height: 44))
         passWord.delegate = self
         passWord.layer.cornerRadius = 5;
         passWord.layer.borderColor = UIColor.lightGrayColor().CGColor;
@@ -100,12 +79,32 @@ class FSLoginViewController: UIViewController , UITextFieldDelegate {
         passWord.leftView?.addSubview(imgPass)
         loginView.addSubview(passWord)
         
+        //手rect1
+        let rectLeftHand = CGRect(x: mainSize.width / 2 - 100, y: loginView.frame.origin.y - 22, width: 40, height: 40)
+        let rectRightHand = CGRect(x: mainSize.width / 2 + 62, y: loginView.frame.origin.y - 22, width: 40, height: 40)
+        //左手图1
+        let leftHandImg = UIImageView(frame: rectLeftHand)
+        leftHandImg.image = UIImage(named: "icon_hand")
+        view.addSubview(leftHandImg)
+        
+        //右手图1
+        let rightHandImg = UIImageView(frame: rectRightHand)
+        rightHandImg.image = UIImage(named: "icon_hand")
+        view.addSubview(rightHandImg)
+        
+        //手rect2
+        let rectLeftHandGone = CGRect(x: 61 - offsetLeftHand, y: 90, width: 40, height: 65)
+        let rectRightHandGone = CGRect(x: loginImg.frame.size.width / 2 + 60, y: 90, width: 40, height: 65)
         
         //左手图2
         let leftHandGoneImg = UIImageView(frame: rectLeftHandGone)
+        leftHandGoneImg.image = UIImage(named: "owl-login-arm-left")
+        loginImg.addSubview(leftHandGoneImg)
         
         //右手图2
         let rightHandGoneImg = UIImageView(frame: rectRightHandGone)
+        rightHandGoneImg.image = UIImage(named: "owl-login-arm-right")
+        loginImg.addSubview(rightHandGoneImg)
         
         let showType :FsLoginTypeShow?
         
